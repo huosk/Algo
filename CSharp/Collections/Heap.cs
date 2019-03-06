140f97eecab01b3ccdd0521e5f2329fcdb064988
huosk <huoshaokai@gmail.com>
2019-03-06 23:37:00 +0800

    up

---
+++ CSharp/Collections/Heap.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algo.Collections
+{
+    public class Heap<T>
+    {
+        private T[] items;
+        private int count;
+        private IComparer<T> comparer;
+
+        public Heap():this(null,Comparer<T>.Default){}
+
+        public Heap(IEnumerable<T> collection,IComparer<T> comparer)
+        {
+
+        }
+
+        public void Insert(T item)
+        {
+            if(count == items.Length-1){
+                // 堆满了，需要动态扩容
+                return;
+            }
+
+            // 插入到数组
+            items[++count] = item;
+
+            int check = count;
+            int parent = check / 2;
+
+            // 存在父节点，并且比父节点大，则交换位置
+            while(parent > 0 && comparer.Compare(items[check],items[parent]) > 0)
+            {
+                T temp = items[parent];
+                items[parent] = items[check];
+                items[check] = temp;
+
+                check = parent;
+                parent = parent / 2;
+            }
+        }
+    }
+}
